bdf951630cbf161fe9ffae3358cd378f2bf21683
wal615 <xwang279@uic.edu>
2020-05-08 10:59:12 -0400

    -inital

---
+++ R/data.R
@@ -0,0 +1,23 @@
+#' A random vector generated from normal distribution
+#'
+#' Some description of the data
+#'
+#' @format A numeric vector of length 100
+#' \describe{
+#'   \item{V1}{...}
+#'   \item{V2}{...}
+#'   ...
+#' }
+"data.x"
+
+#' A random vector generated from normal distribution
+#'
+#' Some description of the data
+#'
+#' @format A numeric vector of length 100
+#' \describe{
+#'   \item{V1}{...}
+#'   \item{V2}{...}
+#'   ...
+#' }
+"data.y"
